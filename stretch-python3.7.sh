4134c208b8463039676838b8b0fdf054567f3a36
kakabomba <kakabomba@gmail.com>
2018-07-03 15:17:38 +0300

    Update stretch-python3.7.sh

--- stretch-python3.7.sh
+++ stretch-python3.7.sh
@@ -2,4 +2,7 @@
 
 echo 'deb http://ftp.de.debian.org/debian testing main' >> /etc/apt/sources.list
 apt-get update
-sudo apt-get -t testing install python3.7 python3.7-venv python3.7-dev
+sudo apt-get -t testing install python3.7 python3.7-venv
+
+sudo apt-get -t testing install python3.7-dev
+sudo apt-get install libxml2-dev libxslt1-dev
