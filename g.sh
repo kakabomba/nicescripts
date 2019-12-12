874c19eeb4e9c0cbc8739db086d042b856828e40
Oles Zaburannyi <ozabu@softserveinc.com>
2019-12-12 16:11:02 +0200

    m

--- g.sh
+++ g.sh
@@ -1,4 +1,4 @@
-git config --global user.email "kakabomba@gmail.com"
+git config --global user.email "kakabomba@protonmail.com"
 git config --global user.name "Oles Zaburannyi"
 git config --global push.default simple
 git config --global core.editor "vim"
